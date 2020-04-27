407bffdcc823494e80a79f23a998a2a527d5c5c1
Piotr Badelek <peter@lifeinmobile.com>
2020-04-27 19:59:08 +0200

    adding npm test as run script, changing names of test specs

--- cypress/support/loginPage.js
+++ cypress/support/loginPage.js
@@ -1,13 +1,13 @@
 Cypress.Commands.add("getErrorDiv", () => cy.get('.fb'))
 
-Cypress.Commands.add("nicknameInput", () => cy.get('[name=username]'))
+Cypress.Commands.add("getUsernameInput", () => cy.get('[name=username]'))
 
 Cypress.Commands.add("getPasswordInput", () => cy.get('[type=password]'))
 
 Cypress.Commands.add("submitForm", () => cy.get('.fw > :nth-child(4) > .bxpad').click())
 
 Cypress.Commands.add("login", (email, password) => {
-	cy.nicknameInput()
+	cy.getUsernameInput()
 	  .type(email)
 	cy.getPasswordInput()
 	  .type(password)
